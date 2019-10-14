80543c36ea86136c6290bceff59390da2fc20229
StGeorge <ingjorgegomez@outlook.com>
2019-10-14 02:35:36 -0500

    Changed the project in order to work with scrapy instead of beautifulsoup

---
+++ ca_scrapy/ca_scrapy/pipelines.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import pymongo
+
+from scrapy.exceptions import DropItem
+
+class CaScrapyPipeline(object):
+    def __init__(self, mongo_server, mongo_port, mongo_db, mongo_collection):
+        self.mongo_server = mongo_server
+        self.mongo_port = mongo_port
+        self.mongo_db = mongo_db
+        self.mongo_collection = mongo_collection
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_server = crawler.settings.get('MONGODB_SERVER'),
+            mongo_port = crawler.settings.get('MONGODB_PORT'),
+            mongo_db = crawler.settings.get('MONGODB_DB'),
+            mongo_collection = crawler.settings.get('MONGODB_COLLETION')
+        )
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_server, self.mongo_port)
+        self.db = self.client[self.mongo_db]
+
+    def close_spider(self, spider):
+        self.client.close()
+
+    def process_item(self, item, spider):
+        valid = True
+        for data in item:
+            if not data:
+                valid = False
+                raise DropItem("Missing {0}".format(data))
+        if valid:
+            self.db[self.mongo_collection].insert(dict(item))
+        return item
+
---
+++ ca_scrapy/ca_scrapy/spiders/verge.py
@@ -0,0 +1,19 @@
+import scrapy
+from scrapy import Selector
+from ca_scrapy.items import CaScrapyItem
+
+class VergeSpider(scrapy.Spider):
+    name = "verge"
+    def start_requests(self):
+        urls = ["https://www.theverge.com/",]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+    
+    def parse(self, response):
+        for article in response.css('a[data-analytics-link="article"]').getall():
+            article = Selector(text=article)
+
+            item = CaScrapyItem()
+            item['url'] = article.xpath('//a/@href').get()
+            item['article_name'] = article.css('a::text').get()
+            yield item
